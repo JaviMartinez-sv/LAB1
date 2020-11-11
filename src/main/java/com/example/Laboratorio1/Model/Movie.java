6bb78673358e7a37c067aa13746bdda1a73b42cd
Javier Martinez <javijog@gmail.com>
2020-11-11 19:10:58 +0100

    Ejercicio3

---
+++ src/main/java/com/example/Laboratorio1/Model/Movie.java
@@ -0,0 +1,40 @@
+package com.example.Laboratorio1.Model;
+public class Movie {
+    
+    public Movie() {
+    }
+
+    public Movie(int _id, int _year, String _title) {
+        this.id = _id;
+        this.year = _year;
+        this.title = _title;
+    }
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    private int year;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    private String title;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
