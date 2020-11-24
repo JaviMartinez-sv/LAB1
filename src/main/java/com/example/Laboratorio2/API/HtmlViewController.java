f0a1629d52d9818a886f757cc638830a66c8c621
Javier Martinez <javijog@gmail.com>
2020-11-24 17:24:02 +0100

    ejercicio 3 y 5

--- src/main/java/com/example/Laboratorio2/API/HtmlViewController.java
+++ src/main/java/com/example/Laboratorio2/API/HtmlViewController.java
@@ -2,7 +2,9 @@ package com.example.Laboratorio2.API;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class HtmlViewController {
     
@@ -13,11 +15,14 @@ public class HtmlViewController {
         return vista;
     }
 
-    @GetMapping("/html2")
-    public ModelAndView movie() {
-        ModelAndView vista = new ModelAndView("index2");
-        vista.addObject("movies", MoviesController.movies);
-        return vista;
-    }
 
+
+    @GetMapping("/html/{id}")
+   public ModelAndView movie(@PathVariable("id") int id) {
+      ModelAndView vista = new ModelAndView("index2");
+      vista.addObject("movies", MoviesController.findById(id));
+      return vista;
+
+   }
 }
+
